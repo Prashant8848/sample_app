a234fabbe14fb962b3460554f55ff16aa027915b
Prashant8848 <prashant.shrestha8848@gmail.com>
2018-11-14 17:36:11 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  root 'application#hello'
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  root 'static_pages#home'
 end
