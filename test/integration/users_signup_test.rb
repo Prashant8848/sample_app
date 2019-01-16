92881cbe7ff3fd98f8197bad872265df1bbbe0fb
“Prashant8848” <prashant.shrestha8848@gmail.com>
2019-01-16 12:05:44 +0900

    19/01/16

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,11 +4,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post signup_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.field_with_errors'
+    assert_select 'form[action="/signup"]'
   end
+  
+
 end
