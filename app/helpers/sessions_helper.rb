640bc57aa097260233df9a34e02d0ffa2f0d7d99
Prashant8848 <prashant.shrestha8848@gmail.com>
2019-02-17 17:56:27 +0900

    account-activation without production

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,31 +11,18 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # Returns the user corresponding to the remember token cookie.
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-
   # Returns true if the given user is the current user.
   def current_user?(user)
     user == current_user
   end
 
-  # Returns the user corresponding to the remember token cookie.
+  # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,23 +7,44 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test "index including pagination" do
-    log_in_as(@user)
-    get users_path
-    assert_template 'users/index'
-    assert_select 'div.pagination', count: 2
-    User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-    end
-  end
+  # test "index including pagination" do
+  #   log_in_as(@user)
+  #   get users_path
+  #   assert_template 'users/index'
+  #   assert_select 'div.pagination', count: 2
+  #   User.paginate(page: 1).each do |user|
+  #     if user.activated?
+  #     assert_select 'a[href=?]', user_path(user), text: user.name
+  #     end
+  #   end
+  # end
+
+  # test "index as admin including pagination and delete links" do
+  #   log_in_as(@admin)
+  #   get users_path
+  #   assert_template 'users/index'
+  #   assert_select 'div.pagination'
+  #   first_page_of_users = User.paginate(page: 1)
+  #   first_page_of_users.each do |user|
+  #     assert_select 'a[href=?]', user_path(user), text: user.name
+  #     unless user == @admin
+  #       assert_select 'a[href=?]', user_path(user), text: 'delete'
+  #     end
+  #   end
+  #   assert_difference 'User.count', -1 do
+  #     delete user_path(@non_admin)
+  #   end
+  # end
 
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.first.toggle!(:activated)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
-    first_page_of_users.each do |user|
+    assigns(:users).each do |user|
+      assert user.activated?
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
